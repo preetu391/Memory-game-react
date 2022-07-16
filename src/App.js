ea0ee1500a265eceaa788792300f1cb7fa460231
Priyanshi Porwal <preetu391391@gmail.com>
2022-07-16 19:44:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Typewriter from 'typewriter-effect';
+import Cards from './Components/Cards';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text'><Typewriter
+           options={{
+            strings: ['Memory Game'],
+            autoStart: true,
+            loop: true,
+          }}
+          /></h1>
+          
+          <Cards/>
+          
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card  ({item, id, handleClick}) {
+    const itemClass = item.stat ? " active " + item.stat : "";
+  return (
+    <div className={"card" + itemClass} onClick={() => handleClick(id)}>
+            <img src={item.img} alt="" />
+        </div>
+  )
+}
+
+export default Card
